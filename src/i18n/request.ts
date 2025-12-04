af732f55f8f95e80c506dc0cb7e086af9d3c17b4
Benjamin Astorga <ben.astorga123@gmail.com>
2025-12-03 19:20:33 -0500

    ADD TRANSLATIONS

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -10,18 +10,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   // Load only common messages (nav, common strings) in the layout
   // Page-specific messages will be loaded dynamically in each page
-  // Use explicit imports for each locale so webpack can statically analyze them
-  let commonMessages;
-  switch (locale) {
-    case 'en':
-      commonMessages = (await import('@/messages/en/common.json')).default;
-      break;
-    case 'fr':
-      commonMessages = (await import('@/messages/fr/common.json')).default;
-      break;
-    default:
-      commonMessages = (await import('@/messages/en/common.json')).default;
-  }
+  const commonMessages = (await import(`@/messages/${locale}/common.json`)).default;
 
   return {
     locale,
